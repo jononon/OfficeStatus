c7b45ca1376112e29cce1b34c69a40845d75e58c
Jonathan Damico <jj_damico@yahoo.com>
2016-04-25 20:46:09 -0700

    ¯\_(ツ)_/¯

--- js.js
+++ js.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   $.ajax({
     url: 'https://maps.googleapis.com/maps/api/directions/json?origin=place_id:ChIJSydXgVm8woARrlqYCBU_ZRQ&destination=place_id:ChIJeVT19fK9woAR2y9xeIx-CTo&key=' + key,
     dataType: 'jsonp',
+    jsonpCallback: 'callback',
+    type: 'GET',
     success: function(data) {
       $('#directions_MS').append('Time to Middle School via ' + routes[0].summary + ': ' + routes[0].legs[0].duration.text);
     }
