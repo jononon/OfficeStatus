b928eb5627614189d9a2e24fdcc264ff9bdf47e5
Jonathan Damico <jj_damico@yahoo.com>
2016-04-25 21:19:10 -0700

    TIME

--- js.js
+++ js.js
@@ -12,6 +12,24 @@ $(document).ready(function() {
   }
   $('#title').append("DJ and Jonathan");
 
+  function updateClock() {
+    var now = new Date(), // current date
+        months = ['January', 'February', '...']; // you get the idea
+        time = now.getHours() + ':' + now.getMinutes(), // again, you get the idea
+
+        // a cleaner way than string concatenation
+        date = [now.getDate(),
+                months[now.getMonth()],
+                now.getFullYear()].join(' ');
+
+    // set the content of the element with the ID time to the formatted string
+    document.getElementById('time').innerHTML = [date, time].join(' / ');
+
+    // call this function again in 1000ms
+    setTimeout(updateClock, 1000);
+  }
+  updateClock(); // initial call
+
 
 /*
   var key = 'AIzaSyAcmscSUENKMTKPgTTX9982lWLuFn2DqC0';
