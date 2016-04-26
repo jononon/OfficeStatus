6af876f9e7bd17e8a5a4ca2cd02c5ec2da0e09fe
Jonathan Damico <jj_damico@yahoo.com>
2016-04-25 21:21:24 -0700

    try this

--- js.js
+++ js.js
@@ -14,16 +14,15 @@ $(document).ready(function() {
 
   function updateClock() {
     var now = new Date(), // current date
-        months = ['January', 'February', '...']; // you get the idea
-        time = now.getHours() + ':' + now.getMinutes(), // again, you get the idea
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; // you get the idea
+        time = now.getHours()%12 + ':' + now.getMinutes() + ':' + now.getSeconds(), // again, you get the idea
 
         // a cleaner way than string concatenation
         date = [now.getDate(),
-                months[now.getMonth()],
-                now.getFullYear()].join(' ');
+                months[now.getMonth()]].join(' ');
 
     // set the content of the element with the ID time to the formatted string
-    document.getElementById('time').innerHTML = [date, time].join(' / ');
+    document.getElementById('time').innerHTML = [date, time].join(', ');
 
     // call this function again in 1000ms
     setTimeout(updateClock, 1000);
