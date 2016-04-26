27f2612ace225fd4a3b8186e20c888b5d6174d1d
Jonathan Damico <jj_damico@yahoo.com>
2016-04-25 19:56:56 -0700

    does this work

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var key = 'AIzaSyAcmscSUENKMTKPgTTX9982lWLuFn2DqC0';
   $.ajax({
     url: 'https://maps.googleapis.com/maps/api/directions/json?origin=place_id:ChIJSydXgVm8woARrlqYCBU_ZRQ&destination=place_id:ChIJeVT19fK9woAR2y9xeIx-CTo&key=' + key,
-    dataType: 'jsonp',
+    dataType: 'json',
     success: function(data) {
       $('#directions_MS').append('Time to Middle School via ' + routes[0].summary + ': ' + routes[0].legs[0].duration.text);
     },
